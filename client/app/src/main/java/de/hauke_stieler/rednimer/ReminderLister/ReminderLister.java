ac9aa9d41b24fea9227bed1772bb9d873d95e93c
Hauke Stieler <mail@hauke-stieler.de>
2017-05-30 20:07:51 +0200

    Remove unneccessary code.

--- client/app/src/main/java/de/hauke_stieler/rednimer/ReminderLister/ReminderLister.java
+++ client/app/src/main/java/de/hauke_stieler/rednimer/ReminderLister/ReminderLister.java
@@ -1,14 +1,11 @@
 package de.hauke_stieler.rednimer.ReminderLister;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,7 +16,6 @@ import de.hauke_stieler.rednimer.R;
 /**
  */
 public class ReminderLister extends Fragment {
-//    private OnFragmentInteractionListener mListener;
 
     private ArrayAdapter<Reminder> _listItemAdapter;
 
@@ -32,23 +28,14 @@ public class ReminderLister extends Fragment {
      *
      * @return A new instance of fragment ReminderLister.
      */
-    // TODO: Rename and change types and number of parameters
     public static ReminderLister newInstance() {
         ReminderLister fragment = new ReminderLister();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
 
         ArrayList<Reminder> reminderListItems = new ArrayList<>();
         reminderListItems.add(new Reminder());
@@ -64,41 +51,9 @@ public class ReminderLister extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_reminder_lister, container, false);
 
-        ListView itemListView = (ListView)view.findViewById(R.id.reminderItemListView);
+        ListView itemListView = (ListView) view.findViewById(R.id.reminderItemListView);
         itemListView.setAdapter(_listItemAdapter);
 
         return view;
     }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-//        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
 }
