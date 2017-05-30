a911483fedec74d3ab3e2cfe753930d5934d8f84
Hauke Stieler <mail@hauke-stieler.de>
2017-05-30 21:36:58 +0200

    Add reminder service interface

---
+++ client/app/src/main/java/de/hauke_stieler/rednimer/Common/ServiceInterface/IReminderService.java
@@ -0,0 +1,14 @@
+package de.hauke_stieler.rednimer.Common.ServiceInterface;
+
+import java.util.Date;
+
+import de.hauke_stieler.rednimer.Common.Reminder;
+
+/**
+ * Created by hauke on 30.05.17.
+ */
+public interface IReminderService {
+    void add(Reminder reminder);
+
+    void getAll(Date date);
+}
