97179976cd8249ed6f467289a1382b9f996eabd4
Hauke Stieler <mail@hauke-stieler.de>
2017-06-05 00:59:46 +0200

    Add first idea of UI of the ReminderCreatorActivity

--- client/app/src/main/java/de/hauke_stieler/rednimer/ReminderCreator/View/ReminderCreatorActivity.java
+++ client/app/src/main/java/de/hauke_stieler/rednimer/ReminderCreator/View/ReminderCreatorActivity.java
@@ -1,14 +1,10 @@
 package de.hauke_stieler.rednimer.ReminderCreator.View;
 
-import android.support.v4.view.ViewPager;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import java.util.Date;
 
 import de.hauke_stieler.rednimer.R;
-import de.hauke_stieler.rednimer.ReminderCreator.Adapter.ReminderCreatorPagerAdapter;
 
 public class ReminderCreatorActivity extends AppCompatActivity {
 
